770fd2fd65427156c25e24fd02a4aa550a4dd64e
azozlion <rri7@hotmail.com>
2013-08-22 07:54:15 +0300

    Create ar.php
    90% arabic

---
+++ TravianZ/GameEngine/Lang/ar.php
@@ -0,0 +1,359 @@
+<?php
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                             TRAVIANX                                             //
+//            Only for advanced users, do not edit if you dont know what are you doing!             //
+//                                Made by: Dzoki & Dixie (TravianX)                                 //
+//                              - TravianX = Travian Clone Project -                                //
+//                                 DO NOT REMOVE COPYRIGHT NOTICE!                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+									//                         //
+									//         ENGLISH         //
+									//      Author: Dzoki      //
+									/////////////////////////////
+
+//MAIN MENU
+define("TRIBE1","الرومان");
+define("TRIBE2","الجرمان");
+define("TRIBE3","الأغريق");
+define("TRIBE4","طبية");
+define("TRIBE5","التتار");
+define("TRIBE6","الوحوش");
+
+define("HOME","الصفحة الرئيسية");
+define("INSTRUCT","التعليمات");
+define("ADMIN_PANEL","لوحة التحكم");
+define("MASS_MESSAGE","رسالة الشامل");
+define("LOGOUT","تسجيل الخروج");
+define("PROFILE","الملف الشخصي");
+define("SUPPORT"," الدعم");
+define("UPDATE_T_10","تحديث 10 الأوئل");
+define("SYSTEM_MESSAGE","رسائل النظام");
+define("TRAVIAN_PLUS","ترافيان <b><span class=\"plus_g\">P</span><span class=\"plus_o\">l</span><span class=\"plus_g\">u</span><span class=\"plus_o\">s</span></span></span></b>");
+define("CONTACT","اتصل بنا");
+define("GAME_RULES","قواعد اللعبه");
+
+//MENU
+define("REG","التسجيل");
+define("FORUM","المنتدى");
+define("CHAT","محادثه");
+define("IMPRINT","بصمة");
+define("MORE_LINKS","المزيد من الروابط");
+define("TOUR","جولة لعبه");
+
+
+//ERRORS
+define("USRNM_EMPTY","(لم يتم أدخل اسم المستخدم)");
+define("USRNM_TAKEN","(هذا الأسم مستخدم)");
+define("USRNM_SHORT","(min. ".USRNM_MIN_LENGTH." figures)");
+define("USRNM_CHAR","(أحرف غير صالحة)");
+define("PW_EMPTY","(لم يتم أدخال كلمة المرزم)");
+define("PW_SHORT","(min. ".PW_MIN_LENGTH." figures)");
+define("PW_INSECURE","(كلمة المرورم غير أمنه الرجاء اختيار كلمة مرور أكثر أماناَ)");
+define("EMAIL_EMPTY","(لم يتم أدخال بريد إلكتروني)");
+define("EMAIL_INVALID","(بريد إلكتروني غير صحيح الرجاء ادخل بريد الكتروني  خاص بك)");
+define("EMAIL_TAKEN","(هذا البريد الإلكتروني مستخدم)");
+define("TRIBE_EMPTY","<li>الرجاء اختيار القبلية.</li>");
+define("AGREE_ERROR","<li>عليك أن توافق على قواعد اللعبة والشروط والأحكام العامة من أجل تسجيل.</li>");
+define("LOGIN_USR_EMPTY"," الرجاء قم بكتابة اسم المستخدم.");
+define("LOGIN_PASS_EMPTY","الرجاء قم بكتابة كلمة المرور.");
+define("EMAIL_ERROR","البريد الإلكتروني غير صحيح");
+define("PASS_MISMATCH","كلمة المررو غير صحيحة");
+define("ALLI_OWNER","الرجاء قم بتعيين رئيس للتحالف قبل الحذف");
+define("SIT_ERROR","حاضنة بالفعل تعيين");
+define("USR_NT_FOUND","الاسم ليس موجود.");
+define("LOGIN_PW_ERROR","كلمة المرور خاطئه");
+define("WEL_TOPIC","نصائح ومعلومات مفيدة");
+define("ATAG_EMPTY","علامة فارغة");
+define("ANAME_EMPTY","الاسم فارغ");
+define("ATAG_EXIST","العلامة مأخوذة");
+define("ANAME_EXIST","الأسم مأخوذ");
+define("NOT_OPENED_YET","لم تبدأ اللعبه حتى الأن ");
+define("REGISTER_CLOSED","تم إغلاق التسجيل . من فضلك قم باختيار سيرفر أخر للتسجيل وبدء اللعب وشكراً");
+define("NAME_EMPTY","الرجاء أدخال الأسم");
+define("NAME_NO_EXIST","لايوجد اي مستخدم مع هذا الآسم ");
+define("ID_NO_EXIST","لايوجد أي مستخدم مع هذا المعرف ");
+define("SAME_NAME","لا تستةيع دعوت نفسك");
+define("ALREADY_INVITED"," مدعو بالأصل");
+define("ALREADY_IN_ALLY"," هو اصلاً في التحالف");
+
+//COPYRIGHT
+define("TRAVIAN_COPYRIGHT","TravianZ 100% Open Source Travian Clone.");
+
+//BUILD.TPL
+define("CUR_PROD","الأنتاج الحالي");
+define("NEXT_PROD","الأنتاج في مستوى ");
+
+//BUILDINGS
+define("B1","الحطاب");
+define("B1_DESC","الحطاب يقوم بقطع الأشجار لإنتاج الخشب. كلما تم تطوير الحطاب كلما ازداد إنتاجه من الخشب.");
+define("B2","حفرة الطين");
+define("B2_DESC","الطين يستخرج من حفرة الطين. كلما تم تطوير حفرة الطين كلما إزداد إنتاج الطين.");
+define("B3","منجم الحديد");
+define("B3_DESC","عمال المناجم يقوموا بإستخراج الحديد الثمين من المنجم. كلما إرتفع مستوى منجم الحديد كلما ازداد إنتاج الحديد.");
+define("B4","حقل القمح");
+define("B4_DESC","في حقول القمح يتم إنتاج الغذاء لسكان القرية، كلما تم تطوير حقول القمح كلما ازداد إنتاج القمح.");
+
+//DORF1
+define("LUMBER","الخشب");
+define("CLAY","الطين");
+define("IRON","الحديد");
+define("CROP","القمح");
+define("LEVEL","مستوى");
+define("CROP_COM",أستهلاك." القمح");
+define("PER_HR","في الساعه");
+define("PROD_HEADER","الإنتاج");
+define("MULTI_V_HEADER","القرى");
+define("ANNOUNCEMENT","إعلان");
+define("GO2MY_VILLAGE","أذهب الى قريتي");
+define("VILLAGE_CENTER","مركز القريه");
+define("FINISH_GOLD","أنجز أوامر البناء والبحث  في هذه القريه بـ 20 قطعة ذهبيه");
+define("WAITING_LOOP","(في أنتظار حلقة)");
+define("HRS","(ساعة.)");
+define("DONE_AT","ينتهي في");
+define("CANCEL","الغاء");
+define("LOYALTY","ولاء:");
+define("CALCULATED_IN","يتم الحساب في");
+define("SEVER_TIME","الوقت في الخادم:");
+
+//QUEST
+define("Q_CONTINUE","تابع الى المهمة التاليه");
+define("Q_REWARD","جائزتك:");
+define("Q0","مرحبا بك في");
+define("Q0_DESC","أنت الأن زعيم هذه القرية الصغيره سوف أكون المستشار الخاص بك للساعات القليلة المقبله وسآكون يدك اليمنى حتى تستطيع الأعتماد على نفسك في أكمال المشوار وحظاَ موفقاَ لك");
+define("Q0_OPT1","إلى المهه الأولى");
+define("Q0_OPT2","أنظر حولك آنت الان بمفردك في القريه");
+define("Q0_OPT3","عدم اللعب بالمهمات");
+
+define("Q1","المهمة 1: الحطاب");
+define("Q1_DESC","هناك أربع غابات خضراء في قريتك رجاء قم ببناء الحطاب في أحد تلك الغابات فالخشب مهم لقرية جديده لدينا");
+define("Q1_ORDER","Order:<\/p>بناء الحطاب.");
+define("Q1_RESP","نعم، وبهذه الطريقة يمكنك الحصول على مزيد من خشب لقد ساعدت قليلا والانتهاء من النظام على الفور.");
+define("Q1_REWARD","انهاء الحطاب فوراً");
+
+define("Q2","المهمة 2: حقل القمح");
+define("Q2_DESC","سكان القرية لايستطيعون العمل ليس لديهم مايكفي من القمح الرجاء قم بتطوير حقل القمح");
+define("Q2_ORDER","Order:<\/p>رجاء قم بتطوير حقل قمح واحد");
+define("Q2_RESP","ممتاز جداً الان لديك مايكفي من القمح لسكان القرية");
+
+define("Q3","المهمة 3: اسم قريتك ");
+define("Q3_DESC","أنت يمكنك منح قريتك آسم الرجاء أنقر على الملف الشخصي في القائمة اليمنى أختر ثم اختر تعديل المعلومات الشخصية");
+define("Q3_ORDER","Order:<\/p>غير آسم قريتك إلى اسم أخر جميل");
+define("Q3_RESP","نجاح كبير لقد أعجبني الآسم كثيراً كان من الممكن أن يكون آسم لقريتي !");
+
+define("Q4","المهمة 4: اللاعبون الأخرون");
+define("Q4_DESC","في (اسم السيرفر) كنت تلعب مع الأف اللاعبون الأخرين انقر فوق الاحصائية وشاهد رتبتك بين اللأعبين  وادخل ترتيبك بين اللاعبين هنا");
+define("Q4_ORDER","Order:<\/p>ابحث عن رتبتك في الإحصائيات وأدخله هنا.");
+define("Q4_BUTN","أكتملة المهمه");
+define("Q4_RESP","هذا هو بالظبط ترتيبك بين اللأعبين.");
+
+define("Q5","المهمة 5:   اوامر البناء");
+define("Q5_DESC","الرجاء قم ببناء منجم حديد واحد وببناء حفرة للطين واحده ");
+define("Q5_ORDER","Order:<\/p><ul><li>بناء منجم حديده واحد.<\/li><li>بناء حفرة طين واحدة.<\/li><\/ul>");
+define("Q5_RESP","كما لاحظت أن أوامر البناء تستغرق وقتاً طويلا بعض الشئ في (اسم السيرفر) وسوف تستمر بالزيادة والنقصان في الايام القليلة القادمه حتى لو لم تكن متصل وسوف يكون هناك الكثير من الأشياء الجديده بالنسبه لك لاأكتشافها.
+\r\n<br \/><br \/>\r\n كان افضل شئ يمكن فعله هو اعطائك مهام جديده للقيام بها
+.
+");
+define("Q5","رسالة من مدير المهام");
+define("Q5_DESC","كنت تعلم أنه كان بانتظارك جائزه رائعة كلم مدير المهام لتحصل على الجائزة هذه الرسالة تلقائيه فلا تقم بالرد عليها ");
+
+//======================================================//
+//================ UNITS - DO NOT EDIT! ================//
+//======================================================//
+define("U0","Hero");
+
+//ROMAN UNITS
+define("U1","جندي أول");
+define("U2","حراس الأمبراطور");
+define("U3","جندي مهاجم");
+define("U4"," فرقة تجسس");
+define("U5"," سلاح الفرسان");
+define("U6"," فرسان قيصر");
+define("U7","كبش");
+define("U8","المقلاع الناري");
+define("U9","حكيم");
+define("U10","مستوطن");
+
+//TEUTON UNITS
+define("U11","مقاتل بهرواة");
+define("U12","مقاتل برمح");
+define("U13","مقاتل بفأس");
+define("U14","الكشاف");
+define("U15","مقاتل القيصر");
+define("U16","فرسان الجرمان");
+define("U17","محطمة الأبواب");
+define("U18","المقلاع");
+define("U19","الزعيم");
+define("U20","مستوطن");
+
+//GAUL UNITS
+define("U21","الكتيبة");
+define("U22","مبارز");
+define("U23","المستكشف");
+define("U24"," رعد الإغريق");
+define("U25","فرسان السلت");
+define("U26","فرسان الهيدوانر");
+define("U27","محطمة الأبواب الخشبية");
+define("U28","المقلاع الحربي");
+define("U29","رئيس");
+define("U30","مستوطن");
+define("U99","Trap");
+
+//NATURE UNITS
+define("U31","Rat");
+define("U32","Spider");
+define("U33","Snake");
+define("U34","Bat");
+define("U35","Wild Boar");
+define("U36","Wolf");
+define("U37","Bear");
+define("U38","Crocodile");
+define("U39","Tiger");
+define("U40","Elephant");
+
+//NATARS UNITS
+define("U41","Pikeman");
+define("U42","Thorned Warrior");
+define("U43","Guardsman");
+define("U44","Birds Of Prey");
+define("U45","Axerider");
+define("U46","Natarian Knight");
+define("U47","War Elephant");
+define("U48","Ballista");
+define("U49","Natarian Emperor");
+define("U50","Natarian Settler");
+
+//MONSTER UNITS
+define("U51","Monster Peon");
+define("U52","Monster Hunter");
+define("U53","Monster Warrior");
+define("U54","Ghost");
+define("U55","Monster Steed");
+define("U56","Monster War Steed");
+define("U57","Monster Ram");
+define("U58","Monster Catapult");
+define("U59","Monster Chief");
+define("U60","Monster Settler");
+
+// RESOURCES
+define("R1","خشب");
+define("R2","طين");
+define("R3","حديد");
+define("R4","قمح");
+
+//INDEX.php
+define("LOGIN","تسجيل الدخول");
+define("PLAYERS","اللاعبين");
+define("ACTIVE","نشط");
+define("ONLINE","المتصلون");
+define("TUTORIAL","البرنامج التعليمي");
+define("PLAYER_STATISTICS","احصائيات اللاعب");
+
+define("LEARN_MORE2","الأن مع ثورة <BR> النظام الجديد كليا <BR> والرسومات <BR> هذا هو العالم الممتع");
+define("COMUNITY","مجتمع");
+define("BECOME_COMUNITY","تصبح جزءا من مجتمعنا الآن!");
+define("BECOME_COMUNITY2","إن تصبح واحدا <BR> من افضل مجتمع للألعاب <BR> في <BR> العالم.");
+define("NEWS","News");
+define("SCREENSHOTS","لقطات");
+define("LEARN1","ترقية الحقول والمناجم لزيادة إنتاج الموارد الخاصة بك. وسوف تحتاج إلى موارد لبناء المباني وتدريب الجنود.");
+define("LEARN2","إنشاء وتوسيع المباني في قريتك. لتحسين البنية التحتية العامة الخاصة بك، وزيادة إنتاج الموارد الخاصة بك ويسمح لك للبحث وتدريب وترقية القوات الخاصة بك.");
+define("LEARN3","أعرض وتفاعل مع محيطك من الجيران. لكي يمكنك تكوين صداقات جديدة أو أعداء جدد، والاستفادة من الواحات القريبة وتلاحظ  نمو الإمبراطورية الخاصه بك وتصبح أقوى.");
+define("LEARN4","تابع التحسينات والنجاحات وقارن بينك وبين اللآعبين الأخرين وذلك عن طريق مشاهدة الأحصائيه لأ افضل 10 لاعبين اسبوعياً والفوز بميدالية تميزك عن بقية اللأعبين الأخرين");
+define("LEARN5","تقارير مفصلة عن مغامراتك،والصفقات والمعارك. لا تنسى التحقق من التقارير  الجديدة حول الأحداث التي تجري في محيطك.يمكنك تلقي ");
+define("LEARN6","تبادل المعلومات وإجراء الدبلوماسية مع لاعبين آخرين. تذكر دائما أن التواصل هو مفتاح الفوزللحصول على اصدقاء جدد وحل الصراعات القديمة.");
+
+define("P_ONLINE","اللاعبين المصتلين: ");
+define("P_TOTAL","مجموع اللاعبين: ");
+define("CHOOSE","رجاء أختر احد السيرفرات المتاحه");
+define("STARTED"," بداية تشغيل اللعبه ". round((time()-COMMENCE)/86400) ." منذ ايام.");
+
+//ANMELDEN.php
+define("NICKNAME","الأسم المستعار");
+define("EMAIL","البريد الإلكتروني");
+define("PASSWORD","كلمة المرور");
+define("ROMANS","رومان");
+define("TEUTONS","جرمان");
+define("GAULS","أغريق");
+define("NW","شمال غرب");
+define("NE"," شمال شرق");
+define("SW","شمال شرق");
+define("SE","شمال غرب");
+define("RANDOM","عشوائي");
+define("ACCEPT_RULES"," أوافق على قواعد اللعبة والشروط والأحكام العامة");
+define("ONE_PER_SERVER","كل لاعب يمكن أمتلاك حساب واحد فقط في كل سيرفر نظام تجنب تعدد العضويات يعمل تلقائياً لطرد اللأعب الذي يملك حسابين وأكثر");
+define("BEFORE_REGISTER","قبل تسجيل الحساب يجب عليك قراءة الآتي  <a href='../anleitung.php' target='_blank'>تعليمات</a> من اسم السيرفر لمعرفة مزايا وعيوب محددة من القبائل الثلاث.");
+define("BUILDING_UPGRADING","Building:");
+define("HOURS","hours");
+
+
+//ATTACKS ETC.
+define("TROOP_MOVEMENTS","تحركات القوات:");
+define("ARRIVING_REINF_TROOPS","القوات القادمة المساندة");
+define("ARRIVING_REINF_TROOPS_SHORT","تعزيز");
+define("OWN_ATTACKING_TROOPS","قواتك المهاجمه");
+define("ATTACK","مهاجمة");
+define("OWN_REINFORCING_TROOPS","القوات الخاصه المساندهههه");
+define("TROOPS_DORF","القوات:");
+
+
+//LOGIN.php
+define("COOKIES","نشكركم على الزيارة
+
+في حالة تعدد المستخدمين لنفس جهاز الكمبيوتر فينبغي عليك مسح ملفات التعريف من جهازك حتى يتم مسح اسم المستعمل وكلمة السر للمحافظة على سرية معلوماتك");
+define("NAME","أسم");
+define("PW_FORGOTTEN","نسيت كلمة المرور؟");
+define("PW_REQUEST","يمكنك أسترجاع كلمة المرور عن طريق أرسالها إلى عنوان بريدك الألكتروني  الذي أستخدمته للتسجيل");
+define("PW_GENERATE","أنشاء كلمة مرور جديدة.");
+define("EMAIL_NOT_VERIFIED","البريد الالكتروني غير مسجل!");
+define("EMAIL_FOLLOW","اتبع هذا الرابط لتفعيل حسابك.");
+define("VERIFY_EMAIL","التحقق من البريد الإلكتروني.");
+define("SERVER_STARTS_IN","اللعبه ستبدأ في: ");
+define("START_NOW","الأن تم البدء");
+
+//404.php
+define("NOTHING_HERE","الرابط خطأ");
+define("WE_LOOKED","الصفحة 404 بالفعل لآيمكن على آي شئ");
+
+//TIME RELATED
+define("CALCULATED","يتم الحساب في");
+define("SERVER_TIME","وقت الخادم:");
+
+//MASSMESSAGE.php
+define("MASS","محتوى الرسالة");
+define("MASS_SUBJECT","الموضوغ:");
+define("MASS_COLOR","لون الرساله:");
+define("MASS_REQUIRED","جميع الحقول مطلوبه");
+define("MASS_UNITS","الصور (وحدة):");
+define("MASS_SHOWHIDE","اخفاء/اعرض");
+define("MASS_READقراءة هذا: بعد اضافة الوجوه التعبيريه يجيب عليك اضافة رقم بعدد الصور الى اليمين او اليسار والا لن تعمل الصور");
+define("MASS_CONFIRM","تأكيد");
+define("MASS_REALLY","هل تريد حقا لإرسال IGM الشامل؟");
+define("MASS_ABORT","ايقاف الأن");
+define("MASS_SENT","IGM تم الإرسال الشامل ");
+
+/*
+|--------------------------------------------------------------------------
+|   Index
+|--------------------------------------------------------------------------
+*/
+
+	   $lang['index'][0][1] = "Welcome to ";
+           $lang['index'][0][2] = "كتيب";
+	   $lang['index'][0][3] = "العب الأن, مجانا!";
+	   $lang['index'][0][4] = "ماهو اسم السيرفر . ";
+	   $lang['index'][0][5] = " اسم السيرفر  هي <b>لعبة متصفح</b> وهي تصور العالم القديم والتعامل مع الألاف من اللآعبين الحقيقين الآخرين</p><p>.<strong>العب مجانأ</strong> بدون ان يتطلب <strong>  ذلك للتحميل   </strong>.";
+	   $lang['index'][0][6] = "انقر هنا للعب ";
+	   $lang['index'][0][7] = "مجموع اللآعبين";
+	   $lang['index'][0][8] = "الأعبين النشطين";
+	   $lang['index'][0][9] = "اللاعبين المتصلين";
+	   $lang['index'][0][10] = "حول اللعبه";
+	   $lang['index'][0][11] = "سوف تبدأ في منصب رئيس قرية صغيرة وسوف تسعى الى تطويرهآ وان تصبح الآقوى لتتمكن من السيطره والهيمنه على العالم من حولك";
+	   $lang['index'][0][12] = "بناء القرى، شن الحروب او إنشاء طرق التجارة مع الجيران.";
+	   $lang['index'][0][13] = "لعب مع وضد الآلاف من اللاعبين الحقيقيين الآخرين وقهر عالم ترافيان.";
+	   $lang['index'][0][14] = "ألأخبار";
+	   $lang['index'][0][15] = "أسئلة وأجوبة";
+	   $lang['index'][0][16] = "لقطات";
+	   $lang['forum'] = "المنتدى";
+	   $lang['register'] = "التسجيل";
+	   $lang['login'] = "تسجيل الدخول";
+?>
