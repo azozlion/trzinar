651a9684c3909a1149b51ee7946726b551be94b2
azozlion <rri7@hotmail.com>
2013-08-22 07:55:51 +0300

    Update ar.php
    90% arabic

--- TravianZ/GameEngine/Lang/ar.php
+++ TravianZ/GameEngine/Lang/ar.php
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                             TRAVIANX                                             //
 //            Only for advanced users, do not edit if you dont know what are you doing!             //
-//                                Made by: Dzoki & Dixie (TravianX)                                 //
+//                                Made by: Dzoki & Dixie (TravianX) & Lion (translate to Arabic)                                //
 //                              - TravianX = Travian Clone Project -                                //
 //                                 DO NOT REMOVE COPYRIGHT NOTICE!                                  //
 //////////////////////////////////////////////////////////////////////////////////////////////////////
